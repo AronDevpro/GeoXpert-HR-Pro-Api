440ec0bd3276a9a2f295e5152a53d1216a977fb3
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2025-01-19 00:53:02 +0530

    fix employee

--- features/employee/employee.controller.js
+++ features/employee/employee.controller.js
@@ -1,12 +1,37 @@
-import {softDelete} from "../../service/commonCrud.js";
+import {softDelete, statusChange} from "../../service/commonCrud.js";
 import {Employee} from "./employee.schema.js";
-import {createEmployee, searchAllEmployees, updateEmployee} from "./employee.service.js";
+import {
+    createEmployee,
+    getAllEmployee,
+    getEmployeesById,
+    searchAllEmployees,
+    updateEmployee
+} from "./employee.service.js";
 
 const model = Employee
 
-export const getAllEmployee = async (req, res) => {
+export const getAllEmployees = async (req, res) => {
+    try {
+        const Employee = await getAllEmployee(req.query);
+        res.status(200).json(Employee);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const searchAllEmployeesBasedOnBranch = async (req, res) => {
     try {
         const Employee = await searchAllEmployees(req.query);
+        res.status(200).json(Employee);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const getEmployeeById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const Employee = await getEmployeesById(id);
         res.status(200).json(Employee);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -27,6 +52,16 @@ export const putEmployee = async (req, res) => {
         const id = req.params.id
         const Employee = await updateEmployee(id,req.body);
         res.status(200).json({ data: Employee , message: "Data Updated." });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const changeEmployeeStatus = async (req, res) => {
+    try {
+        const id = req.params.id
+        const data = await statusChange(model,id, req.body);
+        res.status(200).json({ data: data , message: "Data Updated." });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
--- features/employee/employee.route.js
+++ features/employee/employee.route.js
@@ -1,18 +1,35 @@
 import express from "express";
-import {deleteEmployee, getAllEmployee, postEmployee, putEmployee} from "./employee.controller.js";
+import {
+    changeEmployeeStatus,
+    deleteEmployee,
+    getAllEmployees,
+    getEmployeeById,
+    postEmployee,
+    putEmployee,
+    searchAllEmployeesBasedOnBranch
+} from "./employee.controller.js";
 
 //creating a router
 const router = express.Router();
 
 //api to get all the data
-router.get('/', getAllEmployee);
+router.get('/', getAllEmployees);
+
+//api to get all the data
+router.get('/search', searchAllEmployeesBasedOnBranch);
+
+// gte by id
+router.get('/:id', getEmployeeById);
 
 //api to save
 router.post('/', postEmployee)
 
 //api to update
 router.put('/:id', putEmployee)
-//
+
+//api to change status
+router.put('/status/:id', changeEmployeeStatus)
+
 //api to delete
 router.delete('/:id', deleteEmployee)
 
