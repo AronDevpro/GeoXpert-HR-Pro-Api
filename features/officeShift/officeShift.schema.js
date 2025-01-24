0147ca530780c6f403afa6087991f4cebf93c047
AronDevpro <140414272+AronDevpro@users.noreply.github.com>
2025-01-25 00:18:16 +0530

    Update .gitignore to stop tracking unnecessary files

--- features/officeShift/officeShift.schema.js
+++ features/officeShift/officeShift.schema.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const officeShiftsSchema = new mongoose.Schema({
     name:{type:String,unique:true,required:true},
+    startTime:{type:String,required:true},
+    endTime:{type:String,required:true},
     description:String,
-    status: {type:String,default:"Active"},
+    status: {type:String,enum:["Active","Inactive"],default:"Active"},
 },{ timestamps: true });
 
 export const OfficeShift = mongoose.model("OfficeShift",officeShiftsSchema);
